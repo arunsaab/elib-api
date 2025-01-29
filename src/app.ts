0417756c246bde6ea961414bb045b3f52df2ab4c
arunsaab <arunsinghsaab98@gmail.com>
2025-01-30 01:33:09 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
-import express from "express";
-// import createHttpError from "http-errors"; // Ensure this is installed and imported
+import express, { NextFunction, Request, Response } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+};
+
+app.use(logger);
 
 app.get("/", (req, res) => {
   // const error = createHttpError(404, "Not Found");
   // throw error;
   res.json({ message: "Hello World" });
 });
+
+app.use("/api/users", userRouter);
 
 // Use the error handler correctly
 app.use(globalErrorHandler);
