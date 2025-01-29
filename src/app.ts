895a869a15f72ca79f8e34d511ccf9666351d9af
arunsaab <arunsinghsaab98@gmail.com>
2025-01-30 00:59:32 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+// import createHttpError from "http-errors"; // Ensure this is installed and imported
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req, res) => {
+  // const error = createHttpError(404, "Not Found");
+  // throw error;
   res.json({ message: "Hello World" });
 });
+
+// Use the error handler correctly
+app.use(globalErrorHandler);
 
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,8 @@ conf();
 
 const _config = {
   port: process.env.PORT,
-  mongo_connection_url: process.env.MONGO_CONNECTION_STRING
+  mongo_connection_url: process.env.MONGO_CONNECTION_STRING,
+  env: process.env.NODE_ENV
 };
 
 export const config = Object.freeze(_config);
