84084057e5c443de0b062597f422f3e1885b553d
arunsaab <arunsinghsaab98@gmail.com>
2025-01-29 22:32:00 +0530

    Setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("welcome to my api");
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+};
+
+startServer();
