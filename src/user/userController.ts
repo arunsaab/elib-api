0b32b81a7303e6752a1465f091acb59e00fa5396
arunsaab <arunsinghsaab98@gmail.com>
2025-01-30 12:09:23 +0530

    create new user

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
 import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -17,6 +18,14 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "User with this email already present");
     return next(error);
   }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashPassword
+  });
 
   res.json({
     message: "User Registered"
