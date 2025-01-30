504f91b5c6631bc17902a1ccd9724bbac0ae2a97
arunsaab <arunsinghsaab98@gmail.com>
2025-01-31 00:26:59 +0530

    add book model

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
 const app = express();
 app.use(express.json());
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 
 // Use the error handler correctly
 app.use(globalErrorHandler);
