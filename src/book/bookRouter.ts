631e27e2ae7a43f0c3b64eac63a5ec9e368d29a8
arunsaab <arunsinghsaab98@gmail.com>
2025-02-01 09:50:52 +0530

    setup multer for multipart file

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,8 +1,23 @@
+import path from "node:path";
 import express from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
 
 const bookRouter = express.Router();
 
-bookRouter.post("/", createBook);
+// file storing to local
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploadd"),
+  limits: { fileSize: 3e7 } // 30mb
+});
+
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 }
+  ]),
+  createBook
+);
 
 export default bookRouter;
